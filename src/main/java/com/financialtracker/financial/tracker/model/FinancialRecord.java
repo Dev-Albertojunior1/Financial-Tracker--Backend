6525490e2a717c8572105d42196bce2ad311aead
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-15 23:29:24 +0300

    first commit

--- src/main/java/com/financialtracker/financial/tracker/model/FinancialRecord.java
+++ src/main/java/com/financialtracker/financial/tracker/model/FinancialRecord.java
@@ -1,4 +1,33 @@
 package com.financialtracker.financial.tracker.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+
+
+@Entity
+@Data
 public class FinancialRecord {
+
+  @Id
+  
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String description;
+  private Double amount;
+  private String type; //depends 
+  
 }
+
+
+
+
+
+//description;
+ //amount;
+ //type;
