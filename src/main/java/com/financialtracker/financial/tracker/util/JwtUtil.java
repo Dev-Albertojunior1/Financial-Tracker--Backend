4a0290916f90d8e53f3f0b73b11e155be380df4f
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-16 21:16:58 +0300

    c

--- src/main/java/com/financialtracker/financial/tracker/util/JwtUtil.java
+++ src/main/java/com/financialtracker/financial/tracker/util/JwtUtil.java
@@ -15,39 +15,6 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private String secret = "yourSecretKeyHere";
-    private long expiration = 3600000; // 1 hour
-
-    public String generateToken(User user) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, user.getUsername());
-    }
-
-    private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
-    }
-
-    public boolean validateToken(String token, User user) {
-        final String username = extractUsername(token);
-        return (username.equals(user.getUsername()) && !isTokenExpired(token));
-    }
-
-    private String extractUsername(String token) {
-        return extractAllClaims(token).getSubject();
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractAllClaims(token).getExpiration().before(new Date());
-    }
+    
 }
 
