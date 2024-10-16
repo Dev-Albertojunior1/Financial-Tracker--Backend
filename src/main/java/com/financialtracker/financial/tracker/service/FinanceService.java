667aab85cee3ab1aa2fb7c1a745b9192c6366832
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-16 20:50:56 +0300

    ch

--- src/main/java/com/financialtracker/financial/tracker/service/FinanceService.java
+++ src/main/java/com/financialtracker/financial/tracker/service/FinanceService.java
@@ -1,13 +1,28 @@
 package com.financialtracker.financial.tracker.service;
 
+import com.financialtracker.financial.tracker.model.FinanceRecord;
+import com.financialtracker.financial.tracker.model.User;
 import com.financialtracker.financial.tracker.repository.FinanceRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FinanceService {
 
     @Autowired
     private FinanceRecordRepository financeRecordRepository;
+
+
+    private User getCurrentUser(){
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public List<FinanceRecord> getAllRecords(){
+        return financeRecordRepository.findByUser(getCurrentUser());
+    }
+
     
 }
