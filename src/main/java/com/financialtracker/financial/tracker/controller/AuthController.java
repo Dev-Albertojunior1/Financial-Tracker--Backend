7580d5f7b0ea9bd24df255755d31f6d577e47c1d
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-16 21:15:47 +0300

    changes

---
+++ src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.financialtracker.financial.tracker.controller;
+
+
+import com.financialtracker.financial.tracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+
+}
