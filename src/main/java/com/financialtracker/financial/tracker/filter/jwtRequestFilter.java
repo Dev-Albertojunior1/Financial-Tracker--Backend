4ffc76d084ff034fe97d66a58ccea1bd7e8a2609
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-18 12:16:41 +0300

    commits

--- src/main/java/com/financialtracker/financial/tracker/filter/jwtRequestFilter.java
+++ src/main/java/com/financialtracker/financial/tracker/filter/jwtRequestFilter.java
@@ -1,5 +1,7 @@
 package com.financialtracker.financial.tracker.filter;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -7,21 +9,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.financialtracker.financial.tracker.service.UserDetailsServiceImpl;
 import com.financialtracker.financial.tracker.util.JwtUtil;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 @Component
-public class jwtRequestFilter extends OncePerRequestFilter {
+public class JwtRequestFilter extends OncePerRequestFilter {
 
-  @Autowired
-  private JwtUtil jwtUtil;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-  @Autowired
-  private UserDetailsServiceImpl userDetailsServiceImpl;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse)
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
 
+        String username = null;
+        String jwt = null;
 
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, (User) userDetails)) {
+                // Authentication logic here
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
