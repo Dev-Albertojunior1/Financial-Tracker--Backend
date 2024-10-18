1f0d1d7de90c1582cea46f6163cea0daa8459b95
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-18 12:04:49 +0300

    changes

--- src/main/java/com/financialtracker/financial/tracker/filter/jwtRequestFilter.java
+++ src/main/java/com/financialtracker/financial/tracker/filter/jwtRequestFilter.java
@@ -1,4 +1,27 @@
 package com.financialtracker.financial.tracker.filter;
 
-public class wtRequestFilter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.financialtracker.financial.tracker.service.UserDetailsServiceImpl;
+import com.financialtracker.financial.tracker.util.JwtUtil;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class jwtRequestFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JwtUtil jwtUtil;
+
+  @Autowired
+  private UserDetailsServiceImpl userDetailsServiceImpl;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse)
+
+
 }
