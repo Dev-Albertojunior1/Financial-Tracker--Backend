6fab4cb85b84033a73f9535348ea947fd00f7ed1
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-26 22:05:00 +0300

    changes

--- src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
+++ src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+    
 
     @Autowired
     private UserRepository userRepository;
-     
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
