996375ed9536c956a6f2dc39dcb263aab00032a3
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-26 22:06:08 +0300

    hanges

--- src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
+++ src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+     
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -25,7 +25,7 @@ public class AuthController {
 
     @Autowired
     private JwtUtil jwtUtil;
- 
+
 
 
 
