b4b638143d6e1dbb214e55fe9f721b59b8f596ff
Alberto <albertojunior848423803@gmail.com>
2024-11-01 04:33:55 +0300

    commits

--- src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
+++ src/main/java/com/financialtracker/financial/tracker/controller/AuthController.java
@@ -9,10 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
